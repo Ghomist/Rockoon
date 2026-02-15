384391b47c5ce92699d70b2560e6f6a5179fcc36
Ghomist <ghomist@gmail.com>
2026-02-15 23:49:10 +0800

    chore: upgrade ts target to es2022

--- vite.config.ts
+++ vite.config.ts
@@ -29,6 +29,9 @@ export default defineConfig(async () => ({
       ignored: ["**/src-tauri/**"]
     }
   },
+  build: {
+    target: "es2022"
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src")
